69bf57f32035b13f52a9e50edd95a2dedd1ae778
Jeff Hui <jeff@jeffhui.net>
2014-06-04 20:08:03 -0700

    Initial Commit

---
+++ Tailor/Tailor.h
@@ -0,0 +1,17 @@
+//
+//  Tailor.h
+//  Tailor
+//
+//  Created by Jeff Hui on 6/4/14.
+//  Copyright (c) 2014 Jeff Hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tailor.
+FOUNDATION_EXPORT double TailorVersionNumber;
+
+//! Project version string for Tailor.
+FOUNDATION_EXPORT const unsigned char TailorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tailor/PublicHeader.h>
