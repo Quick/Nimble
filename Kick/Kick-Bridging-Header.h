41498a8e6eb9071150b3a9d1aee1dca81d900f2d
Jeff Hui <jeff@jeffhui.net>
2014-06-28 14:14:29 -0700

    Inline bridging header information to work around bug in swift compilers with frameworks.

--- Kick/Kick-Bridging-Header.h
+++ Kick/Kick-Bridging-Header.h
@@ -1,1 +1,10 @@
-#import "KICExceptionCapture.h"
+// #import "KICExceptionCapture.h"
+// BUG: we have to inline this import because Swift can't import through a bridging header inside a framework
+#import <Foundation/Foundation.h>
+
+@interface KICExceptionCapture : NSObject
+
+- (id)initWithHandler:(void(^)(NSException *))handler finally:(void(^)())finally;
+- (void)tryBlock:(void(^)())unsafeBlock;
+
+@end
--- Kick/Kick.h
+++ Kick/Kick.h
@@ -1,4 +1,4 @@
 #import <Foundation/Foundation.h>
 
-FOUNDATION_EXPORT double KickVersionNumber;
-FOUNDATION_EXPORT const unsigned char KickVersionString[];
+FOUNDATION_EXPORT double KickVersionNumber = 1.0;
+FOUNDATION_EXPORT const unsigned char KickVersionString[] = "1.0.0";
