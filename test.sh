952fe92beba9c2d15ff1bd053e289a874af1132b
Jeff Hui <jeff@jeffhui.net>
2014-08-07 23:58:15 -0700

    Removed unused code in test shell script

--- test.sh
+++ test.sh
@@ -5,22 +5,13 @@ BUILD_DIR=`pwd`/build
 
 set -e
 
-function run_tests { # scheme, sdk, Debug/Release, destination
-    echo "======="
-    echo xcodebuild -project Nimble.xcodeproj -scheme $1 -destination "$4" -configuration $3 -sdk $2 clean build test -destination-timeout 5
-    echo "======="
-    osascript -e 'tell app "iOS Simulator" to quit'
-    if [ -z "$XCPRETTY" ]; then
-        xcodebuild -project Nimble.xcodeproj -scheme "Nimble-iOS" -configuration "Debug" -sdk "iphonesimulator8.0" -destination "name=iPhone 5s,OS=8.0" -destination-timeout 5 clean build test
-    else
-        set -o pipefail && xcodebuild $args | $XCPRETTY -c
-    fi
-}
-
 function test {
     echo "Running ALL iOS and OSX"
 
     set -x
+    osascript -e 'tell app "iOS Simulator" to quit'
+    xcodebuild -project Nimble.xcodeproj -scheme "Nimble-iOS" -configuration "Debug" -sdk "iphonesimulator8.0" -destination "name=iPad Air,OS=8.0" -destination-timeout 5 build test
+
     osascript -e 'tell app "iOS Simulator" to quit'
     xcodebuild -project Nimble.xcodeproj -scheme "Nimble-iOS" -configuration "Debug" -sdk "iphonesimulator8.0" -destination "name=iPhone 5s,OS=8.0" -destination-timeout 5 build test
 
