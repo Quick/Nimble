a7c96e6875844659d4d98fba1152988ec66bc2b6
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-11-10 13:49:23 -0800

    Exit polling early if condition is already met

--- Nimble/Utils/Poll.swift
+++ Nimble/Utils/Poll.swift
@@ -5,8 +5,13 @@ func _pollBlock(#pollInterval: NSTimeInterval, #timeoutInterval: NSTimeInterval,
     var pass: Bool
     do {
         pass = expression()
+        if pass {
+            break
+        }
+
         let runDate = NSDate().dateByAddingTimeInterval(pollInterval) as NSDate
         NSRunLoop.mainRunLoop().runUntilDate(runDate)
-    } while(!pass && NSDate().timeIntervalSinceDate(startDate) < timeoutInterval);
+    } while(NSDate().timeIntervalSinceDate(startDate) < timeoutInterval);
+
     return pass
 }
