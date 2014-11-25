0a20dd63db1572ad2a64e9e2780dc1ca3b7a0507
Jeff Hui <jeff@jeffhui.net>
2014-11-24 20:10:03 -0800

    Increase timeout for async test.
    To make sure travis doesn't fail (since it's a bit slower).

--- NimbleTests/objc/CompatibilityTest.m
+++ NimbleTests/objc/CompatibilityTest.m
@@ -31,7 +31,7 @@
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         obj = @1;
     });
-    expect(obj).withTimeout(2).toEventuallyNot(beNil());
+    expect(obj).withTimeout(5).toEventuallyNot(beNil());
 }
 
 - (void)testBeAnInstanceOf {
