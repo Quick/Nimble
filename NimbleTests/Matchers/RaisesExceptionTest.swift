6702337f2783ccc455e034cdb15258077f74e0ab
Jeff Hui <jeff@jeffhui.net>
2015-01-25 02:16:20 -0800

    Added ability to assert on raised exceptions' userInfo.
    Closes #12.

--- NimbleTests/Matchers/RaisesExceptionTest.swift
+++ NimbleTests/Matchers/RaisesExceptionTest.swift
@@ -2,29 +2,47 @@ import XCTest
 import Nimble
 
 class RaisesExceptionTest: XCTestCase {
-    var exception = NSException(name: "laugh", reason: "Lulz", userInfo: nil)
+    var exception = NSException(name: "laugh", reason: "Lulz", userInfo: ["key": "value"])
 
-    func testCapturingInAutoClosure() {
-        expect(exception.raise()).to(raiseException(named: "laugh"))
+    func testPositiveMatchesInAutoClosure() {
         expect(exception.raise()).to(raiseException())
-
-        failsWithErrorMessage("expected to raise exception named <foo>") {
-            expect(self.exception.raise()).to(raiseException(named: "foo"))
-        }
+        expect(exception.raise()).to(raiseException(named: "laugh"))
+        expect(exception.raise()).to(raiseException(named: "laugh", reason: "Lulz"))
+        expect(exception.raise()).to(raiseException(named: "laugh", reason: "Lulz", userInfo: ["key": "value"]))
     }
 
-    func testCapturingInExplicitClosure() {
+    func testPositiveMatchesInInExplicitClosure() {
+        expect {
+            self.exception.raise()
+        }.to(raiseException())
+
         expect {
             self.exception.raise()
         }.to(raiseException(named: "laugh"))
 
+
         expect {
             self.exception.raise()
-        }.to(raiseException())
+        }.to(raiseException(named: "laugh", reason: "Lulz"))
+
+
+        expect {
+            self.exception.raise()
+        }.to(raiseException(named: "laugh", reason: "Lulz", userInfo: ["key": "value"]))
     }
 
-    func testCapturingWithReason() {
-        expect(exception.raise()).to(raiseException(named: "laugh", reason: "Lulz"))
+    func testNegativeMatches() {
+        failsWithErrorMessage("expected to raise exception named <foo>") {
+            expect(self.exception.raise()).to(raiseException(named: "foo"))
+        }
+
+        failsWithErrorMessage("expected to raise exception named <laugh> with reason <bar>") {
+            expect(self.exception.raise()).to(raiseException(named: "laugh", reason: "bar"))
+        }
+
+        failsWithErrorMessage("expected to raise exception named <laugh> with reason <Lulz> and userInfo <{k = v;}>") {
+            expect(self.exception.raise()).to(raiseException(named: "laugh", reason: "Lulz", userInfo: ["k": "v"]))
+        }
 
         failsWithErrorMessage("expected to raise any exception") {
             expect(self.exception).to(raiseException())
@@ -32,18 +50,21 @@ class RaisesExceptionTest: XCTestCase {
         failsWithErrorMessage("expected to not raise any exception") {
             expect(self.exception.raise()).toNot(raiseException())
         }
-        failsWithErrorMessage("expected to raise exception named <laugh> and reason <Lulz>") {
+        failsWithErrorMessage("expected to raise exception named <laugh> with reason <Lulz>") {
             expect(self.exception).to(raiseException(named: "laugh", reason: "Lulz"))
         }
 
-        failsWithErrorMessage("expected to raise exception named <bar> and reason <Lulz>") {
+        failsWithErrorMessage("expected to raise exception named <bar> with reason <Lulz>") {
             expect(self.exception.raise()).to(raiseException(named: "bar", reason: "Lulz"))
         }
         failsWithErrorMessage("expected to not raise exception named <laugh>") {
             expect(self.exception.raise()).toNot(raiseException(named: "laugh"))
         }
-        failsWithErrorMessage("expected to not raise exception named <laugh> and reason <Lulz>") {
+        failsWithErrorMessage("expected to not raise exception named <laugh> with reason <Lulz>") {
             expect(self.exception.raise()).toNot(raiseException(named: "laugh", reason: "Lulz"))
+        }
+        failsWithErrorMessage("expected to not raise exception named <laugh> with reason <Lulz> and userInfo <{key = value;}>") {
+            expect(self.exception.raise()).toNot(raiseException(named: "laugh", reason: "Lulz", userInfo: ["key": "value"]))
         }
     }
 }
