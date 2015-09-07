c8165e2d50255939f69950ad73f144537bb86c2f
Markus Kauppila <markus.kauppila@gmail.com>
2015-09-07 21:41:32 +0300

    Test failure cases of haveCount matcher

--- NimbleTests/Matchers/HaveCountTest.swift
+++ NimbleTests/Matchers/HaveCountTest.swift
@@ -5,5 +5,13 @@ class HaveCountTest: XCTestCase {
     func testEquality() {
         expect([1, 2, 3]).to(haveCount(3))
         expect([1, 2, 3]).notTo(haveCount(1))
+
+        failsWithErrorMessage("expected to have [1, 2, 3] with count 3, got 1") {
+            expect([1, 2, 3]).to(haveCount(1))
+        }
+
+        failsWithErrorMessage("expected to not have [1, 2, 3] with count 3, got 3") {
+            expect([1, 2, 3]).notTo(haveCount(3))
+        }
     }
 }
