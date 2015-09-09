7f37a9acc7fe0472c65b4d780bdf83389c817b98
Markus Kauppila <markus.kauppila@gmail.com>
2015-09-09 21:56:15 +0300

    Add Objective-C DSL for haveCount matcher

--- Nimble/Matchers/HaveCount.swift
+++ Nimble/Matchers/HaveCount.swift
@@ -12,3 +12,12 @@ public func haveCount<T: Equatable>(expectedValue: Int) -> MatcherFunc<[T]> {
         }
     }
 }
+
+extension NMBObjCMatcher {
+    public class func haveCountMatcher(expected: NSNumber) -> NMBObjCMatcher {
+        return NMBObjCMatcher(canMatchNil: false) { actualExpression, failureMessage in
+            let expr = actualExpression.cast({ $0 as? [NSObject] })
+            return try! haveCount(expected.integerValue).matches(expr, failureMessage: failureMessage)
+        }
+    }
+}
