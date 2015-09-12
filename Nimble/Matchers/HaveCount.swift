b7a6a2a34b283d4b929b877014cf79573b151bc4
Markus Kauppila <markus.kauppila@gmail.com>
2015-09-12 13:43:05 +0300

    Expand type support of haveCount
    In Swift, haveCount can handle all types conforming to
    CollectionType protocol. In Objective-C the matcher can
    handle NSArray, NSDictionary, NSSet and NSHashTable.

--- Nimble/Matchers/HaveCount.swift
+++ Nimble/Matchers/HaveCount.swift
@@ -1,9 +1,23 @@
 import Foundation
 
-
-/// A Nimble matcher that succeeds when the actual array's count equals
+/// A Nimble matcher that succeeds when the actual CollectionType's count equals
 /// the expected value
-public func haveCount<T: Equatable>(expectedValue: Int) -> MatcherFunc<[T]> {
+public func haveCount<T: CollectionType>(expectedValue: T.Index.Distance) -> NonNilMatcherFunc<T> {
+    return NonNilMatcherFunc { actualExpression, failureMessage in
+        if let actualValue = try actualExpression.evaluate() {
+            failureMessage.postfixMessage = "have \(actualValue) with count \(actualValue.count)"
+            let result = expectedValue == actualValue.count
+            failureMessage.actualValue = "\(expectedValue)"
+            return result
+        } else {
+            return false
+        }
+    }
+}
+
+/// A Nimble matcher that succeeds when the actual collection's count equals
+/// the expected value
+public func haveCount(expectedValue: Int) -> MatcherFunc<NMBCollection> {
     return MatcherFunc { actualExpression, failureMessage in
         if let actualValue = try actualExpression.evaluate() {
             failureMessage.postfixMessage = "have \(actualValue) with count \(actualValue.count)"
@@ -19,8 +33,16 @@ public func haveCount<T: Equatable>(expectedValue: Int) -> MatcherFunc<[T]> {
 extension NMBObjCMatcher {
     public class func haveCountMatcher(expected: NSNumber) -> NMBObjCMatcher {
         return NMBObjCMatcher(canMatchNil: false) { actualExpression, failureMessage in
-            let expr = actualExpression.cast({ $0 as? [NSObject] })
-            return try! haveCount(expected.integerValue).matches(expr, failureMessage: failureMessage)
+            let location = actualExpression.location
+            let actualValue = try! actualExpression.evaluate()
+            if let value = actualValue as? NMBCollection {
+                let expr = Expression(expression: ({ value as NMBCollection}), location: location)
+                return try! haveCount(expected.integerValue).matches(expr, failureMessage: failureMessage)
+            } else if let actualValue = actualValue {
+                failureMessage.postfixMessage = "get type of NSArray, NSSet, NSDictionary, or NSHashTable"
+                failureMessage.actualValue = "\(NSStringFromClass(actualValue.dynamicType))"
+            }
+            return false
         }
     }
 }
