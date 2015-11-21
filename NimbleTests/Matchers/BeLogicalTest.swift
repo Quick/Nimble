c8f6a2ae68b48905c6995addf5b31094bfb0dafc
Stephen Celis <stephen@stephencelis.com>
2015-11-21 09:38:56 -0500

    Add protocol-based `expectation.to.be{Tru{e,thy},Fals{e,y}}`
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- NimbleTests/Matchers/BeLogicalTest.swift
+++ NimbleTests/Matchers/BeLogicalTest.swift
@@ -1,25 +1,153 @@
 import XCTest
 import Nimble
 
-enum ConvertsToBool : BooleanType, CustomStringConvertible {
-    case TrueLike, FalseLike
+class BeTruthyTest : XCTestCase {
+    func testShouldMatchNonNilTypes() {
+        expect(true as Bool?).to.beTruthy()
+        expect(1 as Int?).to.beTruthy()
+    }
 
-    var boolValue : Bool {
-        switch self {
-        case .TrueLike: return true
-        case .FalseLike: return false
+    func testShouldMatchTrue() {
+        expect(true).to.beTruthy()
+
+        failsWithErrorMessage("expected to not be truthy, got <true>") {
+            expect(true).to.not.beTruthy()
         }
     }
 
-    var description : String {
-        switch self {
-        case .TrueLike: return "TrueLike"
-        case .FalseLike: return "FalseLike"
+    func testShouldNotMatchNilTypes() {
+        expect(false as Bool?).to.not.beTruthy()
+        expect(nil as Bool?).to.not.beTruthy()
+        expect(nil as Int?).to.not.beTruthy()
+    }
+
+    func testShouldNotMatchFalse() {
+        expect(false).to.not.beTruthy()
+
+        failsWithErrorMessage("expected to be truthy, got <false>") {
+            expect(false).to.beTruthy()
+        }
+    }
+
+    func testShouldNotMatchNilBools() {
+        expect(nil as Bool?).to.not.beTruthy()
+
+        failsWithErrorMessage("expected to be truthy, got <nil>") {
+            expect(nil as Bool?).to.beTruthy()
+        }
+    }
+
+    func testShouldMatchBoolConvertibleTypesThatConvertToTrue() {
+        expect(ConvertsToBool.TrueLike).to.beTruthy()
+
+        failsWithErrorMessage("expected to not be truthy, got <TrueLike>") {
+            expect(ConvertsToBool.TrueLike).to.not.beTruthy()
+        }
+    }
+
+    func testShouldNotMatchBoolConvertibleTypesThatConvertToFalse() {
+        expect(ConvertsToBool.FalseLike).to.not.beTruthy()
+
+        failsWithErrorMessage("expected to be truthy, got <FalseLike>") {
+            expect(ConvertsToBool.FalseLike).to.beTruthy()
         }
     }
 }
 
-class BeTruthyTest : XCTestCase {
+class BeTrueTest : XCTestCase {
+    func testShouldMatchTrue() {
+        expect(true).to.beTrue()
+
+        failsWithErrorMessage("expected to not be true, got <true>") {
+            expect(true).to.not.beTrue()
+        }
+    }
+
+    func testShouldNotMatchFalse() {
+        expect(false).to.not.beTrue()
+
+        failsWithErrorMessage("expected to be true, got <false>") {
+            expect(false).to.beTrue()
+        }
+    }
+
+    func testShouldNotMatchNilBools() {
+        failsWithErrorMessageForNil("expected to not be true, got <nil>") {
+            expect(nil as Bool?).to.not.beTrue()
+        }
+
+        failsWithErrorMessageForNil("expected to be true, got <nil>") {
+            expect(nil as Bool?).to.beTrue()
+        }
+    }
+}
+
+class BeFalsyTest : XCTestCase {
+    func testShouldMatchNilTypes() {
+        expect(false as Bool?).to.beFalsy()
+        expect(nil as Bool?).to.beFalsy()
+        expect(nil as Int?).to.beFalsy()
+    }
+
+    func testShouldNotMatchTrue() {
+        expect(true).to.not.beFalsy()
+
+        failsWithErrorMessage("expected to be falsy, got <true>") {
+            expect(true).to.beFalsy()
+        }
+    }
+
+    func testShouldNotMatchNonNilTypes() {
+        expect(true as Bool?).to.not.beFalsy()
+        expect(1 as Int?).to.not.beFalsy()
+    }
+
+    func testShouldMatchFalse() {
+        expect(false).to.beFalsy()
+
+        failsWithErrorMessage("expected to not be falsy, got <false>") {
+            expect(false).to.not.beFalsy()
+        }
+    }
+
+    func testShouldMatchNilBools() {
+        expect(nil as Bool?).to.beFalsy()
+
+        failsWithErrorMessage("expected to not be falsy, got <nil>") {
+            expect(nil as Bool?).to.not.beFalsy()
+        }
+    }
+}
+
+class BeFalseTest : XCTestCase {
+    func testShouldNotMatchTrue() {
+        expect(true).to.not.beFalse()
+
+        failsWithErrorMessage("expected to be false, got <true>") {
+            expect(true).to.beFalse()
+        }
+    }
+
+    func testShouldMatchFalse() {
+        expect(false).to.beFalse()
+
+        failsWithErrorMessage("expected to not be false, got <false>") {
+            expect(false).to.not.beFalse()
+        }
+    }
+
+    func testShouldNotMatchNilBools() {
+        failsWithErrorMessageForNil("expected to be false, got <nil>") {
+            expect(nil as Bool?).to.beFalse()
+        }
+
+        failsWithErrorMessageForNil("expected to not be false, got <nil>") {
+            expect(nil as Bool?).to.not.beFalse()
+        }
+    }
+}
+
+class BeTruthyDeprecatedTest : XCTestCase {
     func testShouldMatchNonNilTypes() {
         expect(true as Bool?).to(beTruthy())
         expect(1 as Int?).to(beTruthy())
@@ -72,7 +200,7 @@ class BeTruthyTest : XCTestCase {
     }
 }
 
-class BeTrueTest : XCTestCase {
+class BeTrueDeprecatedTest : XCTestCase {
     func testShouldMatchTrue() {
         expect(true).to(beTrue())
 
@@ -100,7 +228,7 @@ class BeTrueTest : XCTestCase {
     }
 }
 
-class BeFalsyTest : XCTestCase {
+class BeFalsyDeprecatedTest : XCTestCase {
     func testShouldMatchNilTypes() {
         expect(false as Bool?).to(beFalsy())
         expect(nil as Bool?).to(beFalsy())
@@ -137,7 +265,7 @@ class BeFalsyTest : XCTestCase {
     }
 }
 
-class BeFalseTest : XCTestCase {
+class BeFalseDeprecatedTest : XCTestCase {
     func testShouldNotMatchTrue() {
         expect(true).toNot(beFalse())
 
@@ -161,6 +289,24 @@ class BeFalseTest : XCTestCase {
 
         failsWithErrorMessageForNil("expected to not be false, got <nil>") {
             expect(nil as Bool?).toNot(beFalse())
+        }
+    }
+}
+
+enum ConvertsToBool : BooleanType, CustomStringConvertible {
+    case TrueLike, FalseLike
+
+    var boolValue : Bool {
+        switch self {
+        case .TrueLike: return true
+        case .FalseLike: return false
+        }
+    }
+
+    var description : String {
+        switch self {
+        case .TrueLike: return "TrueLike"
+        case .FalseLike: return "FalseLike"
         }
     }
 }
