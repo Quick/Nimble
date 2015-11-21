8f97b920225a4e748eb723dee4516bd356b39c69
Stephen Celis <stephen@stephencelis.com>
2015-11-21 09:42:56 -0500

    Add protocol-based `expectation.to.endWith()`
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- NimbleTests/Matchers/EndWithTest.swift
+++ NimbleTests/Matchers/EndWithTest.swift
@@ -2,7 +2,44 @@ import XCTest
 import Nimble
 
 class EndWithTest: XCTestCase {
+    func testEndWithPositives() {
+        expect([1, 2, 3]).to.endWith(3)
+        expect([1, 2, 3]).to.not.endWith(2)
 
+        expect("foobar").to.endWith("bar")
+        expect("foobar").to.not.endWith("oo")
+
+        expect(NSString(string: "foobar").description).to.endWith("bar")
+        expect(NSString(string: "foobar").description).to.not.endWith("oo")
+
+        expect(["a", "b"] as [AnyObject]).to.endWith("b")
+        expect(["a", "b"] as [AnyObject]).to.not.endWith("a")
+    }
+
+    func testEndWithNegatives() {
+        failsWithErrorMessageForNil("expected to end with <2>, got <nil>") {
+            expect(nil as [Int]?).to.endWith(2)
+        }
+        failsWithErrorMessageForNil("expected to not end with <2>, got <nil>") {
+            expect(nil as [Int]?).to.not.endWith(2)
+        }
+
+        failsWithErrorMessage("expected to end with <2>, got <[1, 2, 3]>") {
+            expect([1, 2, 3]).to.endWith(2)
+        }
+        failsWithErrorMessage("expected to not end with <3>, got <[1, 2, 3]>") {
+            expect([1, 2, 3]).to.not.endWith(3)
+        }
+        failsWithErrorMessage("expected to end with <atm>, got <batman>") {
+            expect("batman").to.endWith("atm")
+        }
+        failsWithErrorMessage("expected to not end with <man>, got <batman>") {
+            expect("batman").to.not.endWith("man")
+        }
+    }
+}
+
+class EndWithDeprecatedTest: XCTestCase {
     func testEndWithPositives() {
         expect([1, 2, 3]).to(endWith(3))
         expect([1, 2, 3]).toNot(endWith(2))
@@ -38,5 +75,4 @@ class EndWithTest: XCTestCase {
             expect("batman").toNot(endWith("man"))
         }
     }
-
 }
