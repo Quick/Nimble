e8b96aaceb3a12b2a165c7b62fd2cd9c307cad95
Brian Radebaugh <rivukis@gmail.com>
2015-11-30 23:10:02 -0800

    added call with arguments, count, atLeast, and atMost functions / refactored common code / other minor refactors

--- Nimble/Matchers/Call.swift
+++ Nimble/Matchers/Call.swift
@@ -1,18 +1,100 @@
 import Foundation
 
-public func call(function functionName: String) -> FullMatcherFunc<CallRecorder> {
+public func call(function function: String) -> FullMatcherFunc<CallRecorder> {
     return FullMatcherFunc { expression, failureMessage, isNegationTest in
         guard let expressionValue = try expression.evaluate() else {
             return false
         }
         
-        let result = expressionValue.didCall(function: functionName, recordedCallsDescOption: DidCallResultIncludeOption.OnlyOnUnsuccess)
+        let result = expressionValue.didCall(function: function, recordedCallsDescOption: .OnlyOnUnsuccess)
         
         if !result.success {
-            failureMessage.postfixMessage = "call <\(functionName)> from \(expressionValue.dynamicType)"
+            failureMessage.postfixMessage = descriptionOfAttemptedCall(object: expressionValue, function: function, arguments: [], countDescription: "", count: 0)
             failureMessage.actualValue = result.recordedCallsDescription
         }
         
         return result.success
     }
+}
+
+public func call(function function: String, count: Int) -> FullMatcherFunc<CallRecorder> {
+    return FullMatcherFunc { expression, failureMessage, isNegationTest in
+        guard let expressionValue = try expression.evaluate() else {
+            return false
+        }
+        
+        let result = expressionValue.didCall(function: function, count: count, recordedCallsDescOption: .OnlyOnUnsuccess)
+        
+        if !result.success {
+            failureMessage.postfixMessage = descriptionOfAttemptedCall(object: expressionValue, function: function, arguments: [], countDescription: "exactly", count: count)
+            failureMessage.actualValue = result.recordedCallsDescription
+        }
+        
+        return result.success
+    }
+}
+
+public func call(function function: String, atLeast: Int) -> FullMatcherFunc<CallRecorder> {
+    return FullMatcherFunc { expression, failureMessage, isNegationTest in
+        guard let expressionValue = try expression.evaluate() else {
+            return false
+        }
+        
+        let result = expressionValue.didCall(function: function, atLeast: atLeast, recordedCallsDescOption: .OnlyOnUnsuccess)
+        
+        if !result.success {
+            failureMessage.postfixMessage = descriptionOfAttemptedCall(object: expressionValue, function: function, arguments: [], countDescription: "at least", count: atLeast)
+            failureMessage.actualValue = result.recordedCallsDescription
+        }
+        
+        return result.success
+    }
+}
+
+public func call(function function: String, atMost: Int) -> FullMatcherFunc<CallRecorder> {
+    return FullMatcherFunc { expression, failureMessage, isNegationTest in
+        guard let expressionValue = try expression.evaluate() else {
+            return false
+        }
+        
+        let result = expressionValue.didCall(function: function, atMost: atMost, recordedCallsDescOption: .OnlyOnUnsuccess)
+        
+        if !result.success {
+            failureMessage.postfixMessage = descriptionOfAttemptedCall(object: expressionValue, function: function, arguments: [], countDescription: "at most", count: atMost)
+            failureMessage.actualValue = result.recordedCallsDescription
+        }
+        
+        return result.success
+    }
+}
+
+public func call(function function: String, withArguments arguments: [Any]) -> FullMatcherFunc<CallRecorder> {
+    return FullMatcherFunc { expression, failureMessage, isNegationTest in
+        guard let expressionValue = try expression.evaluate() else {
+            return false
+        }
+        
+        let result = expressionValue.didCall(function: function, withArgs: arguments, recordedCallsDescOption: .OnlyOnUnsuccess)
+        
+        if !result.success {
+            failureMessage.postfixMessage = descriptionOfAttemptedCall(object: expressionValue, function: function, arguments: arguments, countDescription: "", count: 0)
+            failureMessage.actualValue = result.recordedCallsDescription
+        }
+        
+        return result.success
+    }
+}
+
+// MARK: Private
+
+private func descriptionOfAttemptedCall(object object: Any, function: String, arguments: [Any], countDescription: String, count: Int) -> String {
+    var description = "call <\(function)> from \(object.dynamicType)"
+    if !arguments.isEmpty {
+        description += " with \(arguments.map{ "\($0)" }.joinWithSeparator(", "))"
+    } else if !countDescription.isEmpty {
+        let pluralism = count == 1 ? "" : "s"
+        description += " \(countDescription) \(count) time\(pluralism)"
+    }
+    
+    return description
 }
--- NimbleTests/Matchers/CallTest.swift
+++ NimbleTests/Matchers/CallTest.swift
@@ -11,13 +11,9 @@ class CallTest : XCTestCase {
         
         func doStuff() { self.recordCall(function: __FUNCTION__) }
         func doStuffWith(string string: String) { self.recordCall(function: __FUNCTION__, arguments: string) }
-        func doMoreStuffWith(int1 int1: Int, int2: Int) { self.recordCall(function: __FUNCTION__, arguments: int1, int2) }
-        func doWeirdStuffWith(string string: String?, int: Int?) { self.recordCall(function: __FUNCTION__, arguments: string, int) }
-        func doCrazyStuffWith(object object: NSObject) { self.recordCall(function: __FUNCTION__, arguments: object) }
-    }
-    
-    func expectDoStuff(object: CallRecorder) {
-        expect(object).to(call(function: "doStuff()"))
+//        func doMoreStuffWith(int1 int1: Int, int2: Int) { self.recordCall(function: __FUNCTION__, arguments: int1, int2) }
+//        func doWeirdStuffWith(string string: String?, int: Int?) { self.recordCall(function: __FUNCTION__, arguments: string, int) }
+//        func doCrazyStuffWith(object object: NSObject) { self.recordCall(function: __FUNCTION__, arguments: object) }
     }
     
     func testCall() {
@@ -36,7 +32,124 @@ class CallTest : XCTestCase {
         let testClass = TestClass()
         testClass.doStuffWith(string: "swift")
         
+        // when
+        let failingTest = { expect(testClass).to(call(function: "doStuff()")) }
+        
         // then
-        failsWithErrorMessage("expected to call <doStuff()> from TestClass, got <doStuffWith(string:) with swift>") { self.expectDoStuff(testClass) }
+        let expectedMessage = "expected to call <doStuff()> from TestClass, got <doStuffWith(string:) with swift>"
+        failsWithErrorMessage(expectedMessage) { failingTest() }
+    }
+    
+    func testCallWithCount() {
+        // given
+        let testClass = TestClass()
+        
+        // when
+        testClass.doStuffWith(string: "string")
+        
+        // then
+        expect(testClass).to(call(function: "doStuffWith(string:)", count: 1))
+    }
+    
+    func testCallWithCountFailureMessage() {
+        // given
+        let testClass = TestClass()
+        testClass.doStuff()
+        
+        // when
+        let failingTest1 = { expect(testClass).to(call(function: "doDifferentStuff()", count: 1)) }
+        let failingTest2 = { expect(testClass).to(call(function: "doStuff()", count: 2)) }
+        
+        // then
+        let expectedMessage1 = "expected to call <doDifferentStuff()> from TestClass exactly 1 time, got <doStuff()>"
+        failsWithErrorMessage(expectedMessage1) { failingTest1() }
+        
+        let expectedMessage2 = "expected to call <doStuff()> from TestClass exactly 2 times, got <doStuff()>"
+        failsWithErrorMessage(expectedMessage2) { failingTest2() }
+    }
+    
+    func testCallWithAtLeast() {
+        // given
+        let testClass = TestClass()
+        
+        // when
+        testClass.doStuffWith(string: "string")
+        
+        // then
+        expect(testClass).to(call(function: "doStuffWith(string:)", atLeast: 1))
+    }
+    
+    func testCallWithAtLeastFailureMessage() {
+        // given
+        let testClass = TestClass()
+        testClass.doStuff()
+        
+        // when
+        let failingTest1 = { expect(testClass).to(call(function: "doDifferentStuff()", atLeast: 1)) }
+        let failingTest2 = { expect(testClass).to(call(function: "doStuff()", atLeast: 2)) }
+        
+        // then
+        let expectedMessage1 = "expected to call <doDifferentStuff()> from TestClass at least 1 time, got <doStuff()>"
+        failsWithErrorMessage(expectedMessage1) { failingTest1() }
+        
+        let expectedMessage2 = "expected to call <doStuff()> from TestClass at least 2 times, got <doStuff()>"
+        failsWithErrorMessage(expectedMessage2) { failingTest2() }
+    }
+    
+    func testCallWithAtMost() {
+        // given
+        let testClass = TestClass()
+        
+        // when
+        testClass.doStuffWith(string: "string")
+        
+        // then
+        expect(testClass).to(call(function: "doStuffWith(string:)", atMost: 1))
+    }
+    
+    func testCallWithAtMostFailureMessage() {
+        // given
+        let testClass = TestClass()
+        testClass.doStuff()
+        testClass.doStuff()
+        testClass.doStuff()
+        testClass.doStuffWith(string: "quick")
+        testClass.doStuffWith(string: "nimble")
+        
+        // when
+        let failingTest1 = { expect(testClass).to(call(function: "doStuffWith(string:)", atMost: 1)) }
+        let failingTest2 = { expect(testClass).to(call(function: "doStuff()", atMost: 2)) }
+        
+        // then
+        let got = "got <doStuff()>, <doStuff()>, <doStuff()>, <doStuffWith(string:) with quick>, <doStuffWith(string:) with nimble>"
+        let expectedMessage1 = "expected to call <doStuffWith(string:)> from TestClass at most 1 time, \(got)"
+        failsWithErrorMessage(expectedMessage1) { failingTest1() }
+        
+        let expectedMessage2 = "expected to call <doStuff()> from TestClass at most 2 times, \(got)"
+        failsWithErrorMessage(expectedMessage2) { failingTest2() }
+    }
+    
+    func testCallWithParameters() {
+        // given
+        let testClass = TestClass()
+        
+        // when
+        testClass.doStuffWith(string: "string")
+        
+        // then
+        expect(testClass).to(call(function: "doStuffWith(string:)", withArguments: ["string"]))
+    }
+    
+    func testCallWithParametersFailureMessage() {
+        // given
+        let testClass = TestClass()
+        testClass.doStuffWith(string: "swift")
+        
+        // when
+        let failingTest = { expect(testClass).to(call(function: "doStuffWith(string:)", withArguments: ["string"])) }
+        
+        // then
+        let expectedMessage = "expected to call <doStuffWith(string:)> from TestClass with string, got <doStuffWith(string:) with swift>"
+        failsWithErrorMessage(expectedMessage) { failingTest() }
     }
 }
