2bea36a4f8e0adfa383bba4a1256c37376ced52e
Brian Croom <brian.s.croom@gmail.com>
2016-01-21 13:26:50 -0500

    Disable matchers that require features unavailable on Linux:
    * `raiseException` - No ObjC, no exceptions!
    * `beAKindOf` - No replacement for `isKindOfClass` right now
    * `match` - NSRegularExpression not yet implemented in Corelibs Foundation

--- Sources/Nimble/Matchers/RaisesException.swift
+++ Sources/Nimble/Matchers/RaisesException.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+#if _runtime(_ObjC)
+
 /// A Nimble matcher that succeeds when the actual expression raises an
 /// exception with the specified name, reason, and/or userInfo.
 ///
@@ -176,3 +178,4 @@ extension NMBObjCMatcher {
         return NMBObjCRaiseExceptionMatcher(name: nil, reason: nil, userInfo: nil, block: nil)
     }
 }
+#endif
