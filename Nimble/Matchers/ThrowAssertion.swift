a97053ab35325c82e15eb5c784581164c8ad6a0d
Dov Frankel <dov@abbey-code.com>
2016-02-04 15:40:59 -0500

    Added throwAssertion() matcher, which uses @mattgallagher's CwlPreconditionTesting framework (Issue #234)

---
+++ Nimble/Matchers/ThrowAssertion.swift
@@ -0,0 +1,30 @@
+import Foundation
+import CwlPreconditionTesting
+
+public func throwAssertion() -> MatcherFunc<Void> {
+    return MatcherFunc { actualExpression, failureMessage in
+        failureMessage.postfixMessage = "throw an assertion"
+        failureMessage.actualValue = nil
+        
+        var succeeded = true
+        
+        let caughtException: BadInstructionException? = catchBadInstruction {
+            do {
+                try actualExpression.evaluate()
+            } catch let error {
+                succeeded = false
+                failureMessage.postfixMessage += "; threw error instead <\(error)>"
+            }
+        }
+
+        if !succeeded {
+            return false
+        }
+        
+        if caughtException == nil {
+            return false
+        }
+        
+        return true
+    }
+}
---
+++ NimbleTests/Matchers/ThrowAssertionTest.swift
@@ -0,0 +1,37 @@
+import XCTest
+import Nimble
+import CwlPreconditionTesting
+
+class ThrowAssertionTest: XCTestCase {
+    
+    func testPositiveMatch() {
+        expect { precondition(false, "Fatal message") }.to(throwAssertion())
+    }
+    
+    func testErrorThrown() {
+        expect { throw NSError(domain: "test", code: 0, userInfo: nil) }.toNot(throwAssertion())
+    }
+    
+    func testPostAssertionCodeNotRun() {
+        var reachedPoint1 = false
+        var reachedPoint2 = false
+        
+        expect {
+            reachedPoint1 = true
+            precondition(false, "condition message")
+            reachedPoint2 = true
+            }.to(throwAssertion())
+        
+        expect(reachedPoint1) == true
+        expect(reachedPoint2) == false
+    }
+    
+    func testNegativeMatch() {
+        var reachedPoint1 = false
+        
+        expect { reachedPoint1 = true }.toNot(throwAssertion())
+        
+        expect(reachedPoint1) == true
+    }
+    
+}
