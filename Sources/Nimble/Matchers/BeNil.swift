434ccaa7b446e935b27f18a9510382880fa40337
Morgan Chen <morganchen12@gmail.com>
2016-04-26 17:37:51 -0700

    add doc comments to NilLiteral type and associated funcs

--- Sources/Nimble/Matchers/BeNil.swift
+++ Sources/Nimble/Matchers/BeNil.swift
@@ -1,7 +1,11 @@
 import Foundation
 
+/// Global constant instance of NilLiteral. Can be used to 
+/// compare against expectations as syntactic sugar for the
+/// `beNil` matcher.
 public let Nil = NilLiteral()
 
+/// An empty type signifying nothing.
 public struct NilLiteral: NilLiteralConvertible {
     public init(nilLiteral: ()) {}
     public init() {}
@@ -10,10 +14,18 @@ public struct NilLiteral: NilLiteralConvertible {
 extension NilLiteral: Equatable {}
 public func ==(lhs: NilLiteral, rhs: NilLiteral) -> Bool { return true }
 
+/// Equality operator overload that can be used to simplify
+/// nil expectations. For a given expectation `expectThing`,
+/// `expectThing == Nil` and `expectThing.to(beNil)` are 
+/// equivalent expressions.
 public func ==<T>(lhs: Expectation<T>, rhs: NilLiteral) {
     lhs.to(beNil())
 }
 
+/// Equality operator overload that can be used to simplify
+/// nonnil expectations. For a given expectation `expectThing`,
+/// `expectThing != Nil` and `expectThing.toNot(beNil)` are
+/// equivalent expressions.
 public func !=<T>(lhs: Expectation<T>, rhs: NilLiteral) {
     lhs.toNot(beNil())
 }
