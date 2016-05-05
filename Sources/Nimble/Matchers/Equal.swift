6e80a419f6b0304bde402dbfefb3d3035b1d247d
Morgan Chen <morganchen12@gmail.com>
2016-05-05 16:35:18 -0700

    use in-place sort for efficiency

--- Sources/Nimble/Matchers/Equal.swift
+++ Sources/Nimble/Matchers/Equal.swift
@@ -74,7 +74,9 @@ public func equal<T>(expectedValue: Set<T>) -> NonNilMatcherFunc<Set<T>> {
 /// A Nimble matcher that succeeds when the actual set is equal to the expected set.
 public func equal<T: Comparable>(expectedValue: Set<T>) -> NonNilMatcherFunc<Set<T>> {
     return equal(expectedValue, stringify: {
-        return stringify(Array($0).sort(<))
+        var output = Array($0)
+        output.sortInPlace(<)
+        return stringify(output)
     })
 }
 
