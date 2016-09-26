1394720c99d8bc579e2367985d5bbe957e2087f7
Syo Ikeda <suicaicoca@gmail.com>
2016-09-26 13:24:04 +0900

    Use let

--- Sources/Nimble/Utils/Async.swift
+++ Sources/Nimble/Utils/Async.swift
@@ -3,8 +3,8 @@ import Dispatch
 import Foundation
 
 #if !_runtime(_ObjC)
-    private var NSEC_PER_SEC: CUnsignedLongLong = 1000000000
-    private var NSEC_PER_MSEC: CUnsignedLongLong = 1000000
+    private let NSEC_PER_SEC: CUnsignedLongLong = 1000000000
+    private let NSEC_PER_MSEC: CUnsignedLongLong = 1000000
 #endif
 
 private let timeoutLeeway = DispatchTimeInterval.nanoseconds(Int(NSEC_PER_MSEC))
