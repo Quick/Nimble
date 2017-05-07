23620f831fbf7e57a0425b55fc96e7ffbb143794
Gavan Chan <chan.gavan+github@gmail.com>
2017-05-07 11:39:22 +1000

    Tidy up formatting

--- Tests/NimbleTests/objc/ObjCAllPassTest.m
+++ Tests/NimbleTests/objc/ObjCAllPassTest.m
@@ -18,13 +18,13 @@
 - (void)testNegativeMatches {
     expectFailureMessage(@"expected to all be less than <3>, but failed first at element"
                          " <3> in <[1, 2, 3, 4]>", ^{
-                             expect(@[@1, @2, @3,@4]).to(allPass(beLessThan(@3)));
+                             expect(@[@1, @2, @3, @4]).to(allPass(beLessThan(@3)));
                          });
     expectFailureMessage(@"expected to not all be less than <5>", ^{
-        expect(@[@1, @2, @3,@4]).toNot(allPass(beLessThan(@5)));
+        expect(@[@1, @2, @3, @4]).toNot(allPass(beLessThan(@5)));
     });
     expectFailureMessage(@"expected to not all be less than <5>", ^{
-        expect([NSSet setWithArray:@[@1, @2, @3,@4]]).toNot(allPass(beLessThan(@5)));
+        expect([NSSet setWithArray:@[@1, @2, @3, @4]]).toNot(allPass(beLessThan(@5)));
     });
     expectFailureMessage(@"allPass can only be used with types which implement NSFastEnumeration "
                          "(NSArray, NSSet, ...), and whose elements subclass NSObject, got <3>", ^{
