9264daafebc2e400ad2d43b113a3f1fbe5bc1434
Jeff Hui <jeff@jeffhui.net>
2017-05-19 20:26:30 -0700

    Remove extra line

--- Sources/Nimble/Matchers/BeEmpty.swift
+++ Sources/Nimble/Matchers/BeEmpty.swift
@@ -68,7 +68,6 @@ extension NMBObjCMatcher {
             let location = actualExpression.location
             let actualValue = try! actualExpression.evaluate()
 
-
             if let value = actualValue as? NMBCollection {
                 let expr = Expression(expression: ({ value as NMBCollection }), location: location)
                 return try! beEmpty().satisfies(expr).toObjectiveC()
