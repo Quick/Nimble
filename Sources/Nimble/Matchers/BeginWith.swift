609323968d7673bf101f98087766949e6463024c
Adam Nemecek <adamnemecek@gmail.com>
2017-10-19 16:31:11 -0700

    * refactoring beginwith

--- Sources/Nimble/Matchers/BeginWith.swift
+++ Sources/Nimble/Matchers/BeginWith.swift
@@ -4,19 +4,17 @@ import Foundation
 /// is equal to the expected value.
 public func beginWith<S: Sequence, T: Equatable>(_ startingElement: T) -> Predicate<S>
     where S.Iterator.Element == T {
-    return Predicate.simple("begin with <\(startingElement)>") { actualExpression in
-        if let actualValue = try actualExpression.evaluate() {
-            var actualGenerator = actualValue.makeIterator()
-            return PredicateStatus(bool: actualGenerator.next() == startingElement)
-        }
-        return .fail
+    return .simple("begin with <\(startingElement)>") { actualExpression in
+        guard let actualValue = try actualExpression.evaluate() else { return .fail }
+        var actualGenerator = actualValue.makeIterator()
+        return PredicateStatus(bool: actualGenerator.next() == startingElement)
     }
 }
 
 /// A Nimble matcher that succeeds when the actual collection's first element
 /// is equal to the expected object.
 public func beginWith(_ startingElement: Any) -> Predicate<NMBOrderedCollection> {
-    return Predicate.simple("begin with <\(startingElement)>") { actualExpression in
+    return .simple("begin with <\(startingElement)>") { actualExpression in
         guard let collection = try actualExpression.evaluate() else { return .fail }
         guard collection.count > 0 else { return .doesNotMatch }
         #if os(Linux)
@@ -33,12 +31,10 @@ public func beginWith(_ startingElement: Any) -> Predicate<NMBOrderedCollection>
 /// A Nimble matcher that succeeds when the actual string contains expected substring
 /// where the expected substring's location is zero.
 public func beginWith(_ startingSubstring: String) -> Predicate<String> {
-    return Predicate.simple("begin with <\(startingSubstring)>") { actualExpression in
-        if let actual = try actualExpression.evaluate() {
-            let range = actual.range(of: startingSubstring)
-            return PredicateStatus(bool: range != nil && range!.lowerBound == actual.startIndex)
-        }
-        return .fail
+    return .simple("begin with <\(startingSubstring)>") { actualExpression in
+        guard let actual = try actualExpression.evaluate() else { return .fail }
+        let range = actual.range(of: startingSubstring)
+        return PredicateStatus(bool: range?.lowerBound == actual.startIndex)
     }
 }
 
