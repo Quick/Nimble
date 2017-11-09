887c7177efcf6593f7cb70189a0507684085a7f1
mosamer <mostafamamer@gmail.com>
2017-11-09 20:55:45 +0100

    introduce `notTo` expectation extension

--- Sources/Nimble/ExtinsibleExpecation.swift
+++ Sources/Nimble/ExtinsibleExpecation.swift
@@ -19,15 +19,18 @@ import Foundation
 public struct ExtensibleExpecation<Type> {
     public typealias T = Type
     private let base: Expectation<T>
-    init(_ base: Expectation<T>) {
+    private let style: ExpectationStyle
+    init(base: Expectation<T>, style: ExpectationStyle) {
         self.base = base
+        self.style = style
     }
 
     public func match(description: String? = nil, _ predicateFactory: @escaping () -> Predicate<T>) {
+        let to = style == .toMatch ? "to" : "to not"
         let (pass, msg) = execute(base.expression,
-                                  .toMatch,
+                                  style,
                                   predicateFactory(),
-                                  to: "to",
+                                  to: to,
                                   description: description)
         base.verify(pass, msg)
     }
@@ -37,6 +40,16 @@ public struct ExtensibleExpecation<Type> {
 extension Expectation {
     /// Expectation extension to test the actual value using a matcher to match.
     public var to: ExtensibleExpecation<T> {
-        return ExtensibleExpecation(self)
+        return ExtensibleExpecation(base: self, style: .toMatch)
+    }
+    /// Expectation extension to test the actual value using a matcher to not match
+    public var notTo: ExtensibleExpecation<T> {
+        return ExtensibleExpecation(base: self, style: .toNotMatch)
+    }
+    /// Expectation extension to test the actual value using a matcher to not match
+    ///
+    /// Alias to `notTo`.
+    public var toNot: ExtensibleExpecation<T> {
+        return notTo
     }
 }
