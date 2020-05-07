83bf60ac944e15c637865188164c0f195a67e08d
Sho Ikeda <suicaicoca@gmail.com>
2020-05-07 21:43:17 +0900

    Prepare Matcher-to-Predicate migration-path features deprecation
    - Tweak the Deprecation Roadmap in the README
    - Use Predicate in UserDescriptionTest
    - Separate Matcher usages in SynchronousTest into SynchronousDeprecatedTest and mark the entire class deprecated

--- Tests/NimbleTests/SynchronousTest.swift
+++ Tests/NimbleTests/SynchronousTest.swift
@@ -29,38 +29,32 @@ final class SynchronousTest: XCTestCase {
     }
 
     func testToMatchesIfMatcherReturnsTrue() {
-        expect(1).to(MatcherFunc { _, _ in true })
-        expect {1}.to(MatcherFunc { _, _ in true })
-
-        expect(1).to(MatcherFunc { _, _ in true }.predicate)
-        expect {1}.to(MatcherFunc { _, _ in true }.predicate)
-
         expect(1).to(Predicate.simple { _ in .matches })
         expect {1}.to(Predicate.simple { _ in .matches })
     }
 
     func testToProvidesActualValueExpression() {
         var value: Int?
-        expect(1).to(MatcherFunc { expr, _ in value = try expr.evaluate(); return true })
+        expect(1).to(Predicate.simple { expr in value = try expr.evaluate(); return .matches })
         expect(value).to(equal(1))
     }
 
     func testToProvidesAMemoizedActualValueExpression() {
         var callCount = 0
-        expect { callCount += 1 }.to(MatcherFunc { expr, _ in
+        expect { callCount += 1 }.to(Predicate.simple { expr in
             _ = try expr.evaluate()
             _ = try expr.evaluate()
-            return true
+            return .matches
         })
         expect(callCount).to(equal(1))
     }
 
     func testToProvidesAMemoizedActualValueExpressionIsEvaluatedAtMatcherControl() {
         var callCount = 0
-        expect { callCount += 1 }.to(MatcherFunc { expr, _ in
+        expect { callCount += 1 }.to(Predicate.simple { expr in
             expect(callCount).to(equal(0))
             _ = try expr.evaluate()
-            return true
+            return .matches
         })
         expect(callCount).to(equal(1))
     }
@@ -74,49 +68,37 @@ final class SynchronousTest: XCTestCase {
 
     // repeated tests from to() for toNot()
     func testToNotMatchesIfMatcherReturnsTrue() {
-        expect(1).toNot(MatcherFunc { _, _ in false })
-        expect {1}.toNot(MatcherFunc { _, _ in false })
-
-        expect(1).toNot(MatcherFunc { _, _ in false }.predicate)
-        expect {1}.toNot(MatcherFunc { _, _ in false }.predicate)
-
         expect(1).toNot(Predicate.simple { _ in .doesNotMatch })
         expect {1}.toNot(Predicate.simple { _ in .doesNotMatch })
     }
 
     func testToNotProvidesActualValueExpression() {
         var value: Int?
-        expect(1).toNot(MatcherFunc { expr, _ in value = try expr.evaluate(); return false })
+        expect(1).toNot(Predicate.simple { expr in value = try expr.evaluate(); return .doesNotMatch })
         expect(value).to(equal(1))
     }
 
     func testToNotProvidesAMemoizedActualValueExpression() {
         var callCount = 0
-        expect { callCount += 1 }.toNot(MatcherFunc { expr, _ in
+        expect { callCount += 1 }.toNot(Predicate.simple { expr in
             _ = try expr.evaluate()
             _ = try expr.evaluate()
-            return false
+            return .doesNotMatch
         })
         expect(callCount).to(equal(1))
     }
 
     func testToNotProvidesAMemoizedActualValueExpressionIsEvaluatedAtMatcherControl() {
         var callCount = 0
-        expect { callCount += 1 }.toNot(MatcherFunc { expr, _ in
+        expect { callCount += 1 }.toNot(Predicate.simple { expr in
             expect(callCount).to(equal(0))
             _ = try expr.evaluate()
-            return false
+            return .doesNotMatch
         })
         expect(callCount).to(equal(1))
     }
 
     func testToNegativeMatches() {
-        failsWithErrorMessage("expected to match, got <1>") {
-            expect(1).to(MatcherFunc { _, _ in false })
-        }
-        failsWithErrorMessage("expected to match, got <1>") {
-            expect(1).to(MatcherFunc { _, _ in false }.predicate)
-        }
         failsWithErrorMessage("expected to match, got <1>") {
             expect(1).to(Predicate.simple { _ in .doesNotMatch })
         }
@@ -124,19 +106,11 @@ final class SynchronousTest: XCTestCase {
 
     func testToNotNegativeMatches() {
         failsWithErrorMessage("expected to not match, got <1>") {
-            expect(1).toNot(MatcherFunc { _, _ in true })
-        }
-        failsWithErrorMessage("expected to not match, got <1>") {
-            expect(1).toNot(MatcherFunc { _, _ in true }.predicate)
-        }
-        failsWithErrorMessage("expected to not match, got <1>") {
             expect(1).toNot(Predicate.simple { _ in .matches })
         }
     }
 
     func testNotToMatchesLikeToNot() {
-        expect(1).notTo(MatcherFunc { _, _ in false })
-        expect(1).notTo(MatcherFunc { _, _ in false }.predicate)
         expect(1).notTo(Predicate.simple { _ in .doesNotMatch })
     }
 
