f5c5cef3af468e43d9f689bf23cf31eefb68fb3f
Joseph Duffy <commits@josephduffy.co.uk>
2021-06-19 23:40:27 +0100

    Add preliminarily support for watchOS

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Nimble",
     platforms: [
-      .macOS(.v10_10), .iOS(.v9), .tvOS(.v9)
+      .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
     ],
     products: [
         .library(name: "Nimble", targets: ["Nimble"]),
@@ -14,18 +14,18 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "Nimble", 
+            name: "Nimble",
             dependencies: [
                 .product(name: "CwlPreconditionTesting", package: "CwlPreconditionTesting",
                          condition: .when(platforms: [.macOS, .iOS])),
                 .product(name: "CwlPosixPreconditionTesting", package: "CwlPreconditionTesting",
-                         condition: .when(platforms: [.tvOS]))
+                         condition: .when(platforms: [.tvOS, .watchOS]))
             ],
             exclude: ["Info.plist"]
         ),
         .testTarget(
-            name: "NimbleTests", 
-            dependencies: ["Nimble"], 
+            name: "NimbleTests",
+            dependencies: ["Nimble"],
             exclude: ["objc", "Info.plist"]
         ),
     ],
