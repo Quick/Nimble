76912d26e8263af5aeac040ecec31c8a26e0d54b
Rachel Brindle <you@subluminal.net>
2023-04-03 23:37:37 -0700

    Now apply that same behavior to satisfyAnyOf

--- Sources/Nimble/Matchers/SatisfyAnyOf.swift
+++ Sources/Nimble/Matchers/SatisfyAnyOf.swift
@@ -8,10 +8,11 @@ public func satisfyAnyOf<T>(_ predicates: Predicate<T>...) -> Predicate<T> {
 /// provided in the array of matchers.
 public func satisfyAnyOf<T>(_ predicates: [Predicate<T>]) -> Predicate<T> {
         return Predicate.define { actualExpression in
+            let cachedExpression = actualExpression.withCaching()
             var postfixMessages = [String]()
             var status: PredicateStatus = .doesNotMatch
             for predicate in predicates {
-                let result = try predicate.satisfies(actualExpression)
+                let result = try predicate.satisfies(cachedExpression)
                 if result.status == .fail {
                     status = .fail
                 } else if result.status == .matches, status != .fail {
@@ -21,7 +22,7 @@ public func satisfyAnyOf<T>(_ predicates: [Predicate<T>]) -> Predicate<T> {
             }
 
             var msg: ExpectationMessage
-            if let actualValue = try actualExpression.evaluate() {
+            if let actualValue = try cachedExpression.evaluate() {
                 msg = .expectedCustomValueTo(
                     "match one of: " + postfixMessages.joined(separator: ", or "),
                     actual: "\(actualValue)"
