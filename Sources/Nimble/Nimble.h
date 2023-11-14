5cf19cabc597936e6bf70b292d9261dd684db9cb
David Steinacher <david.steinacher@bitmovin.com>
2023-11-14 09:18:58 +0100

    move TARGET_OS_VISION definition to umbrella header

--- Sources/Nimble/Nimble.h
+++ Sources/Nimble/Nimble.h
@@ -1,4 +1,11 @@
 #import <Foundation/Foundation.h>
+
+// When running below Xcode 15, TARGET_OS_VISION is not defined. Since the project has TREAT_WARNINGS_AS_ERROS enabled
+// we need to workaround this warning.
+#ifndef TARGET_OS_VISION
+    #define TARGET_OS_VISION   0
+#endif /* TARGET_OS_VISION */
+
 #import <Nimble/NMBExceptionCapture.h>
 #import <Nimble/NMBStringify.h>
 #import <Nimble/DSL.h>
