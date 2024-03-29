fd62670fbe128927c6dda2614a48cb2680c286db
Rachel Brindle <you@subluminal.net>
2024-03-29 09:55:32 -0700

    Add matchers for Result that match against submatchers, or for equatable values.

--- Sources/Nimble/Matchers/BeResult.swift
+++ Sources/Nimble/Matchers/BeResult.swift
@@ -33,6 +33,60 @@ public func beSuccess<Success, Failure>(
     }
 }
 
+/// A Nimble matcher for Result that succeeds when the actual value is success
+/// and the value inside result is equal to the expected value
+public func beSuccess<Success, Failure>(
+    _ value: Success
+) -> Matcher<Result<Success, Failure>> where Success: Equatable {
+    return Matcher.define { expression in
+        let message = ExpectationMessage.expectedActualValueTo(
+            "be <success(\(Success.self))> that equals \(stringify(value))"
+        )
+
+        guard case let .success(resultValue)? = try expression.evaluate() else {
+            return MatcherResult(status: .doesNotMatch, message: message)
+        }
+
+        return MatcherResult(
+            bool: resultValue == value,
+            message: message
+        )
+    }
+}
+
+/// A Nimble matcher for Result that succeeds when the actual value is success
+/// and the provided matcher matches.
+public func beSuccess<Success, Failure>(
+    _ matcher: Matcher<Success>
+) -> Matcher<Result<Success, Failure>> {
+    return Matcher.define { expression in
+        let message = ExpectationMessage.expectedActualValueTo(
+            "be <success(\(Success.self))> that satisfies matcher"
+        )
+
+        guard case let .success(value)? = try expression.evaluate() else {
+            return MatcherResult(status: .doesNotMatch, message: message)
+        }
+
+        let subExpression = Expression(
+            expression: { value },
+            location: expression.location
+        )
+        let subResult = try matcher.satisfies(subExpression)
+
+        let matches = subResult.toBoolean(expectation: .toMatch)
+
+        return MatcherResult(
+            bool: matches,
+            message: message.appended(
+                details: subResult.message.toString(
+                    actual: stringify(value)
+                )
+            )
+        )
+    }
+}
+
 /// A Nimble matcher for Result that succeeds when the actual value is failure.
 ///
 /// You can pass a closure to do any arbitrary custom matching to the error inside result.
@@ -63,5 +117,38 @@ public func beFailure<Success, Failure>(
         }
 
         return MatcherResult(bool: matches, message: message)
+    }
+}
+
+/// A Nimble matcher for Result that succeeds when the actual value is failure
+/// and the provided matcher matches.
+public func beFailure<Success, Failure>(
+    _ matcher: Matcher<Failure>
+) -> Matcher<Result<Success, Failure>> {
+    return Matcher.define { expression in
+        let message = ExpectationMessage.expectedActualValueTo(
+            "be <failure(\(Failure.self))> that satisfies matcher"
+        )
+
+        guard case let .failure(error)? = try expression.evaluate() else {
+            return MatcherResult(status: .doesNotMatch, message: message)
+        }
+
+        let subExpression = Expression(
+            expression: { error },
+            location: expression.location
+        )
+        let subResult = try matcher.satisfies(subExpression)
+
+        let matches = subResult.toBoolean(expectation: .toMatch)
+
+        return MatcherResult(
+            bool: matches,
+            message: message.appended(
+                details: subResult.message.toString(
+                    actual: stringify(error)
+                )
+            )
+        )
     }
 }
