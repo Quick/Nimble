2ea1ade3ae50c10320b28836b189ea13286bf3fc
Rachel Brindle <you@subluminal.net>
2024-12-11 15:45:47 -0800

    Add an overload of map that allows you to fail the conversion by returning nil.

--- Tests/NimbleTests/Matchers/MapTest.swift
+++ Tests/NimbleTests/Matchers/MapTest.swift
@@ -5,6 +5,7 @@ import NimbleSharedTestHelpers
 #endif
 
 final class MapTest: XCTestCase {
+    // MARK: Map
     func testMap() {
         expect(1).to(map({ $0 }, equal(1)))
 
@@ -66,6 +67,83 @@ final class MapTest: XCTestCase {
         actor Box {
             let int: Int
             let string: String
+
+            init(int: Int, string: String) {
+                self.int = int
+                self.string = string
+            }
+        }
+
+        let box = Box(int: 3, string: "world")
+
+        expect(box).to(satisfyAllOf(
+            map(\.int, equal(3)),
+            map(\.string, equal("world"))
+        ))
+    }
+
+    // MARK: Failable map
+    func testFailableMap() {
+        expect("1").to(map({ Int($0) }, equal(1)))
+
+        struct Value {
+            let int: Int?
+            let string: String?
+        }
+
+        expect(Value(
+            int: 1,
+            string: "hello"
+        )).to(satisfyAllOf(
+            map(\.int, equal(1)),
+            map(\.string, equal("hello"))
+        ))
+
+        expect(Value(
+            int: 1,
+            string: "hello"
+        )).to(satisfyAnyOf(
+            map(\.int, equal(2)),
+            map(\.string, equal("hello"))
+        ))
+
+        expect(Value(
+            int: 1,
+            string: "hello"
+        )).toNot(satisfyAllOf(
+            map(\.int, equal(2)),
+            map(\.string, equal("hello"))
+        ))
+    }
+
+    func testFailableMapAsync() async {
+        struct Value {
+            let int: Int?
+            let string: String?
+        }
+
+        await expect(Value(
+            int: 1,
+            string: "hello"
+        )).to(map(\.int, asyncEqual(1)))
+
+        await expect(Value(
+            int: 1,
+            string: "hello"
+        )).toNot(map(\.int, asyncEqual(2)))
+    }
+
+    func testFailableMapWithAsyncFunction() async {
+        func someOperation(_ value: Int) async -> String? {
+            "\(value)"
+        }
+        await expect(1).to(map(someOperation, equal("1")))
+    }
+
+    func testFailableMapWithActor() {
+        actor Box {
+            let int: Int?
+            let string: String?
 
             init(int: Int, string: String) {
                 self.int = int
