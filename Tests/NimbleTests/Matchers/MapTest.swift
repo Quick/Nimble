c1c63623f4eaca3dd4478cc39bc0e6cc98f30e25
Rachel Brindle <you@subluminal.net>
2024-12-11 16:41:10 -0800

    Rename the failable map overload to compactMap
    And also provide another overload of map that takes in an optional and behaves much more closely to what the original map matcher does

--- Tests/NimbleTests/Matchers/MapTest.swift
+++ Tests/NimbleTests/Matchers/MapTest.swift
@@ -158,4 +158,91 @@ final class MapTest: XCTestCase {
             map(\.string, equal("world"))
         ))
     }
+
+    // MARK: Compact map
+    func testCompactMap() {
+        expect("1").to(compactMap({ Int($0) }, equal(1)))
+        expect("1").toNot(compactMap({ Int($0) }, equal(2)))
+
+        let assertions = gatherExpectations(silently: true) {
+            expect("not a number").to(compactMap({ Int($0) }, equal(1)))
+            expect("not a number").toNot(compactMap({ Int($0) }, equal(1)))
+        }
+
+        expect(assertions).to(haveCount(2))
+        expect(assertions.first?.success).to(beFalse())
+        expect(assertions.last?.success).to(beFalse())
+    }
+
+    func testCompactMapAsync() async {
+        struct Value {
+            let int: Int?
+            let string: String?
+        }
+
+        await expect("1").to(compactMap({ Int($0) }, asyncEqual(1)))
+        await expect("1").toNot(compactMap({ Int($0) }, asyncEqual(2)))
+
+        let assertions = await gatherExpectations(silently: true) {
+            await expect("not a number").to(compactMap({ Int($0) }, asyncEqual(1)))
+            await expect("not a number").toNot(compactMap({ Int($0) }, asyncEqual(1)))
+        }
+
+        expect(assertions).to(haveCount(2))
+        expect(assertions.first?.success).to(beFalse())
+        expect(assertions.last?.success).to(beFalse())
+    }
+
+    func testCompactMapWithAsyncFunction() async {
+        func someOperation(_ value: Int) async -> String? {
+            "\(value)"
+        }
+        await expect(1).to(compactMap(someOperation, equal("1")))
+
+        func someFailingOperation(_ value: Int) async -> String? {
+            nil
+        }
+
+        let assertions = await gatherExpectations(silently: true) {
+            await expect(1).to(compactMap(someFailingOperation, equal("1")))
+            await expect(1).toNot(compactMap(someFailingOperation, equal("1")))
+        }
+
+        expect(assertions).to(haveCount(2))
+        expect(assertions.first?.success).to(beFalse())
+        expect(assertions.last?.success).to(beFalse())
+    }
+
+    func testCompactMapWithActor() {
+        actor Box {
+            let int: Int?
+            let string: String?
+
+            init(int: Int?, string: String?) {
+                self.int = int
+                self.string = string
+            }
+        }
+
+        let box = Box(int: 3, string: "world")
+
+        expect(box).to(satisfyAllOf(
+            compactMap(\.int, equal(3)),
+            compactMap(\.string, equal("world"))
+        ))
+
+        let failingBox = Box(int: nil, string: nil)
+
+        let assertions = gatherExpectations(silently: true) {
+            expect(failingBox).to(satisfyAllOf(
+                compactMap(\.int, equal(3))
+            ))
+            expect(failingBox).toNot(satisfyAllOf(
+                compactMap(\.int, equal(3))
+            ))
+        }
+        expect(assertions).to(haveCount(2))
+        expect(assertions.first?.success).to(beFalse())
+        expect(assertions.last?.success).to(beFalse())
+    }
 }
